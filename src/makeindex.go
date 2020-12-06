a9823c6c809e98be98b4666c9f0fb2027e675f55
Ankit Sambhare <ankitsambhare3004@gmail.com>
2020-12-06 16:30:29 +0530

    development branch change

--- src/makeindex.go
+++ src/makeindex.go
@@ -13,16 +13,10 @@ func main() {
 	}
 	defer file.Close()
 	v := os.Getenv("name")
-	s := `<%@ page import = "java.io.*,java.util.*, javax.servlet.*" %>
-<html>
+	s := `<html>
 <body>
 <h2>Hello `+ v +` the Tomcat server is running!</h2>
-<h1>The curent server time is 
-<%
-         Date date = new Date();
-         out.print( "<h2 align = \"center\">" +date.toString()+"</h2>");
- %>
-</h1>
+<h1>The curent server time is <%= new java.util.Date() %></h1>
 </body>
 </html>`
 	fmt.Fprintf(file, s)
